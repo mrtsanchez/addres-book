df84b30d3c09255a4e8f1550445dcde5c638481e
Marta Sanchez <sanchez.marta@gmail.com>
2016-08-22 23:26:32 -0700

    update color scheme and contscts display

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,16 @@
 // business Logic
 
-function Contact(first, last) {
+function Contact(first, last, email, phone, picture) {
   this.firstName = first;
   this.lastName = last;
+  this.email = email;
+  this.phone = phone;
+  this.picture = picture;
 }
 
 Contact.prototype.fullName = function() {
   return this.firstName + " " + this.lastName;
 }
-
 
 
 // user interface logic
@@ -20,19 +22,33 @@ $(document).ready(function() {
 
       var inputtedFirstName = $("input#new-first-name").val();
       var inputtedLastName = $("input#new-last-name").val();
+      var inputtedEmail = $("input#new-email").val();
+      var inputtedPhone = $("input#new-phone").val();
+      var inputtedPicture = $("input#new-picture").val();
 
-      var newContact = new Contact(inputtedFirstName, inputtedLastName);
+
+      var newContact = new Contact(inputtedFirstName, inputtedLastName, inputtedEmail, inputtedPhone, inputtedPicture);
+
+      $(".address-book").show();
 
       $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() + "</span></li>");
 
       $(".contact").last().click(function() {
+        $(".picture").empty();
         $("#show-contact").show();
-        $("#show-contact h2").text(newContact.firstName);
+        $("#show-contact h2").text(newContact.fullName());
         $(".first-name").text(newContact.firstName);
         $(".last-name").text(newContact.lastName);
+        $(".email").text(newContact.email);
+        $(".phone").text(newContact.phone);
+        $(".picture").append("<img src='" + newContact.picture + "' class='img-responsive' />");
       });
 
       $("input#new-first-name").val("");
       $("input#new-last-name").val("");
+      $("input#new-email").val("");
+      $("input#new-phone").val("");
+      $("input#new-picture").val("");
+
     });
   });
